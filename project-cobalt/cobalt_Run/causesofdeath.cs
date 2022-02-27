8513bb109ffdcf7ff80e2a50cfaaf3a9181fc8d6
MrCryptoFantom <81333581+MrCryptoFantom@users.noreply.github.com>
2022-02-27 03:13:40 -0600

    Main Files

---
+++ project-cobalt/cobalt_Run/causesofdeath.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Cobalt.CausesOfDeath {
+    public class CausesOfDeath {
+
+        public static string[] causesOfDeath = {
+                "a car accident", 
+                "a stab wound", 
+                "a disease", 
+                "a helicopter crash", 
+                "a skydiving accident", 
+                "a landslide", 
+                "drowning in a lake", 
+                "a dog attack", 
+                "a gunshot to the chest", 
+                "cancer of the brain", 
+                "asphyxiation", 
+                "a shark attack while surfing",
+                "a house fire", 
+                "getting hit by a train",
+                "drowning in the city pool",
+                "heart failure",
+                "Ibloo's smelly socks",
+                "a snake bite",
+                "a stray bullet during a police chase",
+                "getting hit by a car",
+                "a boating accident",
+                "a plane crash",
+                "a bashed-in skull",
+                "an assasination",
+                "a Tsunami",
+                "a violent Tornado",
+                "radiation exposure",
+                "a robbery gone bad",
+                "a drive-by shooting",
+                "accidently mixing Bleach and Ammonia",
+                "a 10'th Alabama volley"
+            };
+    }
+}
